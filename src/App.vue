d51178585b785815dd2d20540b462d29a7147fc4
Ondef <dohfawaz90@gmail.com>
2023-08-28 11:30:47 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import { ref } from 'vue';
+
+  let count = ref(0)
+
+
+  let AddCount = ()=>{
+    count.value = count.value + 1
+  }
+
+  let SubCount = ()=>{
+    count.value = count.value - 1
+  }
+</script>
+
+
+<template>
+  <main>
+    <div class='counter'>
+      <div class="cont">
+        <h4 >Le Compteur est a ....</h4>
+      <h1>{{count}}</h1>
+      <button @click="SubCount()">-</button>
+      <button @click="AddCount()">+</button>
+      </div>
+    </div>
+  </main>
+</template>
